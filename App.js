7319f8e9776c00c3d134c9bb953a2de401625bd5
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-21 17:41:52 +0530

    Keyboard fix

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import AddTodo from './components/AddTodo';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
@@ -35,21 +35,27 @@ export default function App() {
     }
   }
 
+  const handleKeyboard = () => {
+    Keyboard.dismiss();
+  }
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo input={input} setInput={setInput} submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={handleKeyboard} >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo input={input} setInput={setInput} submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
